7a82a676e3ca76dd1c8c711ba7f68afc09a6d242
Med-Saiah <mohamedgold776@gmail.com>
2025-05-05 20:38:33 +0100

    temp commit

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import '../styles/Projects.css';
+import portfolioImg from '../assets/portfolio.png';
+import facturationImg from '../assets/facturation.png';
+
+
 import { FaGithub, FaExternalLinkAlt } from 'react-icons/fa';
 
 const Projects = () => {
@@ -12,7 +16,7 @@ const Projects = () => {
 
         {/* Project 1 */}
         <div className="project-card" data-aos="fade-up">
-          <img src="https://via.placeholder.com/400x250" alt="Project 1" className="project-img" />
+          <img src={portfolioImg} alt="Project 1" className="project-img" />
           <h3 className="project-title">Portfolio Website</h3>
           <p className="project-description">
             A personal portfolio website to showcase my projects, skills, and experience with a professional modern design.
@@ -24,7 +28,7 @@ const Projects = () => {
             <a href="https://github.com/your-repo" target="_blank" rel="noopener noreferrer">
               <FaGithub className="link-icon" /> Code
             </a>
-            <a href="https://your-website.com" target="_blank" rel="noopener noreferrer">
+            <a href="https://med-saiah.github.io/portfolio/" target="_blank" rel="noopener noreferrer">
               <FaExternalLinkAlt className="link-icon" /> Demo
             </a>
           </div>
@@ -32,7 +36,7 @@ const Projects = () => {
 
         {/* Project 2 */}
         <div className="project-card" data-aos="fade-up">
-          <img src="https://via.placeholder.com/400x250" alt="Project 2" className="project-img" />
+          <img src={facturationImg} alt="Project 2" className="project-img" />
           <h3 className="project-title">Store Management App</h3>
           <p className="project-description">
             A full CRUD web app to manage products, clients, and invoices with authentication and clean UI.
@@ -41,10 +45,10 @@ const Projects = () => {
             <span>PHP</span> | <span>MySQL</span> | <span>MVC</span>
           </div>
           <div className="project-links">
-            <a href="https://github.com/your-repo" target="_blank" rel="noopener noreferrer">
+            <a href="https://github.com/Med-Saiah/Gestion-de-stock" target="_blank" rel="noopener noreferrer">
               <FaGithub className="link-icon" /> Code
             </a>
-            <a href="https://your-demo-link.com" target="_blank" rel="noopener noreferrer">
+            <a href="#" target="_blank" rel="noopener noreferrer">
               <FaExternalLinkAlt className="link-icon" /> Demo
             </a>
           </div>
